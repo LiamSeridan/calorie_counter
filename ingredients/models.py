1eec1bdc1cde3ba448c9f40ae92b50a2d348eafc
LiamSeridan <paexternal301@oaisb.info>
2024-10-01 01:55:23 -0700

    Revert "Added Category"

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,17 +1,10 @@
 # ingredients/models.py
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
+# ingredients models
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     calories = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
         return self.name
